60d334f82c3b70c8f8ac8ecaf2a2714120753f88
Shayaan Sultan <sultan.shayaan@gmail.com>
2024-01-12 14:35:00 +0500

    Added Navlinks to Layout Navbar

--- frontend-client/src/components/Layout.tsx
+++ frontend-client/src/components/Layout.tsx
@@ -1,8 +1,10 @@
 import { Box, Typography, IconButton, Button, ButtonGroup, Divider} from '@mui/joy';
 import { Brightness7 } from '@mui/icons-material';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
+import { Person } from '@mui/icons-material';
 import { useColorScheme } from '@mui/joy/styles';
 import { Outlet } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Layout = () => {
   const { mode, setMode } = useColorScheme();  
@@ -10,7 +12,9 @@ const Layout = () => {
   return (
     <>
       <Box display="flex" justifyContent={'space-around'} alignItems={'center'} p={3}>
-        <Typography level="h1">Eduforum</Typography>
+        <NavLink to="/" style={{ textDecoration: 'none' }}>
+          <Typography level="h1">Eduforum</Typography>
+        </NavLink>
         <ButtonGroup variant='plain' spacing={2} size='lg'>
           <IconButton
             color="neutral"
@@ -20,7 +24,11 @@ const Layout = () => {
             {useColorScheme().mode === 'dark' ? <Brightness7 /> : <DarkModeIcon />}
           </IconButton>
           <Divider orientation="vertical"  />
-          <Button href="/login">Login</Button>
+          <NavLink to="/user" style={{ textDecoration: 'none' }}>
+            <IconButton>
+              <Person />
+            </IconButton>
+          </NavLink>
         </ButtonGroup>
       </Box>
 

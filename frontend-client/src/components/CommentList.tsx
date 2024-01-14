ae4d93740d7af1a0e8c7e79c4619d2fbeb511ba0
Shayaan Sultan <sultan.shayaan@gmail.com>
2024-01-15 02:23:21 +0800

    Fixed edge case

--- frontend-client/src/components/CommentList.tsx
+++ frontend-client/src/components/CommentList.tsx
@@ -1,3 +1,4 @@
+import { Box, Typography } from '@mui/joy';
 import { Comment } from '../interfaces/Comment';
 import CommentCard from './CommentCard';
 
@@ -11,6 +12,11 @@ const CommentList: React.FC<CommentListProps> = (prop) => {
       {prop.comments.map((comment: Comment) => (
         <CommentCard key={comment.comment_id} comment={comment} />
       ))}
+      {prop.comments.length === 0 && 
+        <Box display="flex" justifyContent={'space-evenly'} alignItems={'center'} p={1.5}>
+          <Typography level="title-md">No comments yet</Typography>
+        </Box>
+      }
     </div>
   );
 };

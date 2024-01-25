3464c47b4f12901aa6050327a729b2e794947fe3
Shayaan Sultan <sultan.shayaan@gmail.com>
2024-01-25 21:26:34 +0800

    Added Categories.sql

---
+++ Categories.sql
@@ -0,0 +1,12 @@
+INSERT INTO Categories (category_id, name) VALUES
+(1, 'General'),
+(2, 'Admin'),
+(3, 'Exams'),
+(4, 'Computer Science'),
+(5, 'Mathematics'),
+(6, 'Business'),
+(7, 'Social Sciences'),
+(8, 'Natural Sciences'),
+(9, 'Societies and Co-curricular Activities'),
+(10, 'Sports'),
+(11, 'Others');
